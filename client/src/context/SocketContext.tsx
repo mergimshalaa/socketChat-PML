c1706b711b7323bbe2c551d49f4b1d470b449bc9
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-03 12:50:29 +0200

    socket context

---
+++ client/src/context/SocketContext.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren, createContext, useState, useContext, useEffect} from "react";
+import { Socket, io } from "socket.io-client";
+import type {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "../../../server/apitypes.ts";
+
+interface ContextValues {
+  socket: Socket;
+}
+
+const SocketContext = createContext<ContextValues>(null as any);
+export const useSocket = () => useContext(SocketContext);
+
+export function SocketProvider({ children }: PropsWithChildren) {
+  const [socket] = useState<Socket<ServerToClientEvents, ClientToServerEvents>>(
+    io()
+  );
+
+  useEffect(() => {
+    function connect() {
+        console.log('connected to server')  
+    }
+    function disconnect() {
+        console.log('disconnected from server')  
+    }
+    function message(message:string) {
+        console.log(message)  
+    }
+    socket.on('connect', connect);
+    socket.on('disconnect', disconnect);
+    socket.on('message', message)
+    return()=> {
+        socket.off('connect', connect)
+        socket.off('disconnect', disconnect)
+        socket.off('message', message)
+    }
+  }, [socket]);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+}
