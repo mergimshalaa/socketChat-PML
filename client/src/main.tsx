69542c80506fcdacc788458ae7831b03be7be80c
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-03 17:04:12 +0200

    implement socketprovider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Socket, io } from 'socket.io-client';
+import type { ClientToServerEvents, ServerToClientEvents } from '../../server/apitypes.ts';
 import App from './App.tsx';
+import { SocketProvider } from './context/SocketContext.tsx';
 import './index.css';
-import type { ServerToClientEvents, ClientToServerEvents } from '../../server/apitypes.ts'
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <SocketProvider>
+      <App />
+    </SocketProvider>
   </React.StrictMode>,
 )
