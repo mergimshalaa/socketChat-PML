44a2c9cdf39127690c6f01eb85b3f225903bd8d5
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-04 12:33:27 +0200

    outcomment for now
    Co-authored-by: Parham Berenjian <ParhamInBinary@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
-import Sidebar from './components/Sidebar'
+// import Sidebar from './components/Sidebar'
 
 function App() {
   return (
      <>
-        <Sidebar />
+        {/* <Sidebar /> */}
     <Router>
         <Route path="/" element={<Home/>} />
         {/* add more routes here */}
