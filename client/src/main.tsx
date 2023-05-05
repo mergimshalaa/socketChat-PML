cad1c944fedddc18af31f55d5783c302a125a814
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-05 16:01:09 +0200

    restructure file
    Co-authored-by: Parham Berenjian <ParhamInBinary@users.noreply.github.com>
    Co-authored-by: Linus Hammarberg <Lvl1Pichu@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Socket, io } from 'socket.io-client';
 import type { ClientToServerEvents, ServerToClientEvents } from '../../server/apitypes.ts';
 import App from './App.tsx';
+import Background from './background/Background.tsx';
 import { SocketProvider } from './context/SocketContext.tsx';
 import './index.css';
-import Background from './background/Background.tsx';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
