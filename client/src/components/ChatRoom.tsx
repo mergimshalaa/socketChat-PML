53890629f2b17fd85f2bcfa980582e358abd514e
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-06 21:42:34 +0200

    styling on messages

--- client/src/components/ChatRoom.tsx
+++ client/src/components/ChatRoom.tsx
@@ -1,24 +1,42 @@
-import {MessageInput} from "../components/MessageInput";
+import styled from "styled-components";
+import { MessageInput } from "../components/MessageInput";
 import { useSocket } from "../context/SocketContext";
 
-
 export function ChatRoom() {
-    const { room, messages } = useSocket();
-    
-    return (
-        <>
-        <header>
-            <h1>You are in room: {room}</h1>
-        </header>
-        <main>
-            <ul>
-                {messages.map((message, i) => (
-                    <li key={i}>{message.name}: {message.message}</li>
-                ))}
-            </ul>
+  const { room, messages } = useSocket();
 
-            <MessageInput />
-        </main>
-        </>
-    )
+  return (
+    <>
+      <header>
+        <h1>You are in room: {room}</h1>
+      </header>
+      <main>
+        <ul>
+          {messages.map((message, i) => (
+            <MessageWrapper key={i}>
+              <SenderName>{message.name} said:</SenderName>
+              <Message> {message.message}</Message>
+            </MessageWrapper>
+          ))}
+        </ul>
+
+        <MessageInput />
+      </main>
+    </>
+  );
 }
+
+const MessageWrapper = styled.div`
+
+`;
+
+const SenderName = styled.div`
+  padding: 0.5rem;
+`;
+
+const Message = styled.div`
+  display: inline-block;
+  background-color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+`;
