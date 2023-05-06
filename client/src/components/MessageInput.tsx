1a97c366116a365eb0a39e5080f330d59add9053
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-06 21:51:39 +0200

    styled message input + button

--- client/src/components/MessageInput.tsx
+++ client/src/components/MessageInput.tsx
@@ -1,21 +1,49 @@
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import styled from "styled-components";
 import { useSocket } from "../context/SocketContext";
 
- export function MessageInput() {
-     const [message, setMessage] = useState('')
-     const { sendMessage } = useSocket();
+export function MessageInput() {
+  const [message, setMessage] = useState("");
+  const { sendMessage } = useSocket();
 
-     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        sendMessage(message);
-        setMessage('');
-     }
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="name" placeholder="Message..." value={message} onChange={(e) => setMessage(e.target.value)}/>
-            <button type="submit">Send</button>
-        </form>
-        </>
-    )
- } 
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    sendMessage(message);
+    setMessage("");
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="name"
+          placeholder="Message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button type="submit">
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </Button>
+      </form>
+    </>
+  );
+}
+
+const Input = styled.input`
+  padding: 0.5rem 1rem;
+  border-radius: 0.3rem;
+  border: none;
+  margin: 0.5rem;
+  `;
+  
+  const Button = styled.button`
+  border: none;
+  padding: 0.5rem;
+  border-radius: 0.3rem;
+  background-color: #2192FF;
+  color: #FFF;
+`;
