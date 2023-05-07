eb2647f6dda6e21b84562817ce7ead07dd00da09
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-07 22:17:57 +0200

    routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,30 @@
-import ReactDOM from 'react-dom/client';
-import { Socket, io } from 'socket.io-client';
-import type { ClientToServerEvents, ServerToClientEvents } from '../../server/apitypes.ts';
-import App from './App.tsx';
-import Background from './background/Background.tsx';
-import { SocketProvider } from './context/SocketContext.tsx';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Socket, io } from "socket.io-client";
+import type {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "../../server/apitypes.ts";
+import App from "./App.tsx";
+import Background from "./background/Background.tsx";
+import { SocketProvider } from "./context/SocketContext.tsx";
+import "./index.css";
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+export const socket: Socket<
+  ServerToClientEvents,
+  ClientToServerEvents
+> = io();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
   // <React.StrictMode>
+  <BrowserRouter>
     <SocketProvider>
       <Background>
         <App />
       </Background>
     </SocketProvider>
+  </BrowserRouter>
   // </React.StrictMode>,
-)
+);
