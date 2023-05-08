4a50f73f65b840d32fae21a789b4d4e63ac7c66b
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-08 11:30:56 +0200

    added logic for list of rooms
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -1,4 +1,10 @@
-import { PropsWithChildren, createContext, useContext, useEffect, useState } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { io } from "socket.io-client";
 import { Message } from "../../../server/apitypes";
 
@@ -6,59 +12,79 @@ interface ContextValues {
   room?: string;
   joinRoom: (room: string, name: string) => void;
   sendMessage: (message: string) => void;
-  messages: Message[]
+  messages: Message[];
+  roomList: string[];
 }
 
 const socket = io();
-const SocketContext = createContext<ContextValues>(null as any);
+const SocketContext = createContext<ContextValues>(
+  null as any
+);
 export const useSocket = () => useContext(SocketContext);
 
-export function SocketProvider({ children }: PropsWithChildren) {
-  const [messages, setMessages] = useState<Message[]>([])
-  const [room, setRoom] = useState<string>('');
-  
-  
-  const joinRoom = ( room: string, name: string ) => {
-    socket.emit('join', room, name, () => {
+export function SocketProvider({
+  children,
+}: PropsWithChildren) {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [room, setRoom] = useState<string>("");
+  const [roomList, setRoomList] = useState<string[]>([]);
+
+  const joinRoom = (room: string, name: string) => {
+    socket.emit("join", room, name, () => {
       setRoom(room);
-      console.log(`User ${name} joined room: ${room}`)
-    })
-  }
+      console.log(`User ${name} joined room: ${room}`);
+    });
+  };
 
   const sendMessage = (message: string) => {
-    if (!room) return console.log('Must enter a room to send message!')
-    socket.emit('message', room, message)
-  }
+    if (!room)
+      return console.log(
+        "Must enter a room to send message!"
+      );
+    socket.emit("message", room, message);
+  };
 
   useEffect(() => {
     function connect() {
-        console.log('connected to server')  
+      console.log("connected to server");
     }
     function disconnect() {
-        console.log('disconnected from server')  
+      console.log("disconnected from server");
     }
-    function message(name: string, message:string) {
-      setMessages((messages) => [...messages, { name, message }])
+    function message(name: string, message: string) {
+      setMessages((messages) => [
+        ...messages,
+        { name, message },
+      ]);
     }
-    function rooms() {
-        console.log(rooms)  
+    function rooms(rooms: string[]) {
+      console.log(rooms);
+      setRoomList(rooms);
     }
 
-    socket.on('connect', connect);
-    socket.on('disconnect', disconnect);
-    socket.on('message', message)
-    socket.on('rooms', rooms)
+    socket.on("connect", connect);
+    socket.on("disconnect", disconnect);
+    socket.on("message", message);
+    socket.on("rooms", rooms);
 
-    return()=> {
-      socket.off('connect', connect)
-      socket.off('disconnect', disconnect)
-      socket.off('message', message)
-      socket.off('rooms', rooms)
-    }
-  },[]);
+    return () => {
+      socket.off("connect", connect);
+      socket.off("disconnect", disconnect);
+      socket.off("message", message);
+      socket.off("rooms", rooms);
+    };
+  }, []);
 
   return (
-    <SocketContext.Provider value={{ room, joinRoom, sendMessage, messages }}>
+    <SocketContext.Provider
+      value={{
+        room,
+        joinRoom,
+        sendMessage,
+        messages,
+        roomList,
+      }}
+    >
       {children}
     </SocketContext.Provider>
   );
