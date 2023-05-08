0a71371602ddc6bc33ae915bc27391705c62d9cd
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-08 11:31:06 +0200

    prettier
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

--- server/server.ts
+++ server/server.ts
@@ -1,36 +1,48 @@
-import { Server } from 'socket.io';
-import type { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from './apitypes';
+import { Server } from "socket.io";
+import type {
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from "./apitypes";
 
-const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>();
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>();
 
-io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
 
-    socket.on('message', (room: string, message: string) => {
-        io.to(room).emit('message', socket.data.name!, message);
-        console.log(room, socket.data.name, message)
-    })
+  socket.on("message", (room: string, message: string) => {
+    io.to(room).emit("message", socket.data.name!, message);
+    console.log(room, socket.data.name, message);
+  });
 
-    socket.on('join', (room: string, name: string, ack) => {
-        socket.data.name = name;
-        socket.join(room);
-        ack();
-        io.emit('rooms', getRooms()) 
-    });
+  socket.on("join", (room: string, name: string, ack) => {
+    socket.data.name = name;
+    socket.join(room);
+    ack();
+    io.emit("rooms", getRooms());
+  });
 
-    socket.emit('rooms', getRooms())
-})
+  socket.emit("rooms", getRooms());
+});
 
 function getRooms() {
-    const { rooms } = io.sockets.adapter;
-    const roomList: string[] = [];
-    for (const [name, setOfSocketids] of rooms) {
-        if (!setOfSocketids.has(name)) {
-            roomList.push(name);
-        }
+  const { rooms } = io.sockets.adapter;
+  const roomList: string[] = [];
+  console.log(rooms);
+
+  for (const [name, setOfSocketids] of rooms) {
+    if (!setOfSocketids.has(name)) {
+      roomList.push(name);
     }
-    return roomList
+  }
+  return roomList;
 }
 
 io.listen(3000);
-console.log('Listening on port 3000');
+console.log("Listening on port 3000");
