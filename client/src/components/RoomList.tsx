33a5fc282d6e971624b389458e976493e8076f06
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-08 12:28:54 +0200

    Styling
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

--- client/src/components/RoomList.tsx
+++ client/src/components/RoomList.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useSocket } from "../context/SocketContext";
 
 export function RoomList() {
@@ -5,9 +6,35 @@ export function RoomList() {
 
   return (
     <>
-      {roomList.map((roomName) => (
-        <div>{roomName}</div>
-      ))}
+      <Wrapper>
+        <RoomTitle>Active rooms:</RoomTitle>
+        <RoomListDiv>
+          {roomList.map((roomName) => (
+            <ListedRoom key={roomName}>{roomName}</ListedRoom>
+          ))}
+        </RoomListDiv>
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  width: 200px;
+  display: inline-block;
+`;
+
+const RoomTitle = styled.h3`
+    margin-left: 1rem;
+`;
+
+const RoomListDiv = styled.div`
+    border-radius: 1rem;
+    background-color: #FFF;
+    padding: .5rem 1rem;;
+    `;
+    
+    const ListedRoom = styled.div`
+    border-bottom: 1px solid lightgray;
+    padding: .2rem;
+    margin: .5rem 0;
+`;
