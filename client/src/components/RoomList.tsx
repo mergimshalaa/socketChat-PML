9c0f72a33832bd4a1e7368e80548c3be4e98b023
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-10 23:33:20 +0200

    togglefunction for sidebar

--- client/src/components/RoomList.tsx
+++ client/src/components/RoomList.tsx
@@ -1,33 +1,47 @@
 import styled from "styled-components";
 import { useSocket } from "../context/SocketContext";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export function RoomList() {
   const { roomList, leaveRoom, joinRoom } = useSocket();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   function handleJoinRoom(roomName: string) {
     leaveRoom();
     joinRoom(roomName);
+    setIsSidebarOpen(false);
+  }
+
+  function handleSidebarToggle() {
+    setIsSidebarOpen(!isSidebarOpen);
   }
 
   return (
-    <Wrapper>
-      <RoomTitle>Active rooms:</RoomTitle>
-      <RoomListDiv>
-        {roomList.map((roomName) => (
-          <ListedRoom
-            key={roomName}
-            id={roomName}
-            onClick={() => handleJoinRoom(roomName)}
-          >
-            {roomName}
-          </ListedRoom>
-        ))}
-      </RoomListDiv>
-    </Wrapper>
+    <>
+      <Wrapper isSidebarOpen={isSidebarOpen}>
+      <ToggleButton onClick={handleSidebarToggle}>
+        {isSidebarOpen ? <FaTimes /> : <FaBars />}
+      </ToggleButton>
+        <RoomTitle>Active rooms:</RoomTitle>
+        <RoomListDiv>
+          {roomList.map((roomName) => (
+            <ListedRoom
+              key={roomName}
+              id={roomName}
+              onClick={() => handleJoinRoom(roomName)}
+            >
+              {roomName}
+            </ListedRoom>
+          ))}
+        </RoomListDiv>
+      </Wrapper>
+    </>
   );
 }
 
-const Wrapper = styled.div`
+
+const Wrapper = styled.div<{ isSidebarOpen: boolean }>`
   width: 200px;
   display: inline-block;
   background-color: #f5f5f5;
@@ -35,6 +49,9 @@ const Wrapper = styled.div`
   border-radius: 1rem;
   box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2),
     -4px -4px 8px rgba(255, 255, 255, 0.2);
+  transform: ${(props) =>
+    props.isSidebarOpen ? "translateX(0)" : "translateX(-100%)"};
+  transition: transform 0.3s ease-in-out;
 `;
 
 const RoomTitle = styled.h3`
@@ -61,3 +78,14 @@ const ListedRoom = styled.div`
   }
 `;
 
+const ToggleButton = styled.button`
+  position: absolute;
+  top: 0.5rem;
+  right: .8rem;
+  background-color: transparent;
+  border: none;
+  font-size: 1.5rem;
+  &:focus {
+    outline: none;
+  }
+`;
