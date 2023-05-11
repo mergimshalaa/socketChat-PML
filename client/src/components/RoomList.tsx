1a3fb55f7ffebbab84cef3a6672c615824050f26
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-11 20:06:00 +0200

    styling

--- client/src/components/RoomList.tsx
+++ client/src/components/RoomList.tsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
-import { useSocket } from "../context/SocketContext";
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import styled from "styled-components";
+import { useSocket } from "../context/SocketContext";
 
 export function RoomList() {
   const { roomList, leaveRoom, joinRoom } = useSocket();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   function handleJoinRoom(roomName: string) {
     leaveRoom();
@@ -19,9 +19,9 @@ export function RoomList() {
   return (
     <>
       <Wrapper isSidebarOpen={isSidebarOpen}>
-      <ToggleButton onClick={handleSidebarToggle}>
-        {isSidebarOpen ? <FaTimes /> : <FaBars />}
-      </ToggleButton>
+        <ToggleButton onClick={handleSidebarToggle}>
+          {isSidebarOpen ? <FaTimes /> : <FaBars />}
+        </ToggleButton>
         <RoomTitle>Active rooms:</RoomTitle>
         <RoomListDiv>
           {roomList.map((roomName) => (
@@ -39,22 +39,27 @@ export function RoomList() {
   );
 }
 
-
 const Wrapper = styled.div<{ isSidebarOpen: boolean }>`
   width: 200px;
   display: inline-block;
   background-color: #f5f5f5;
   padding: 0.3rem;
   border-radius: 1rem;
+  position: absolute;
   box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2),
     -4px -4px 8px rgba(255, 255, 255, 0.2);
   transform: ${(props) =>
-    props.isSidebarOpen ? "translateX(0)" : "translateX(-100%)"};
+    props.isSidebarOpen
+      ? "translateX(0)"
+      : "translateX(-100%)"};
   transition: transform 0.3s ease-in-out;
+  @media (max-width: 480px) {
+    left: 1rem;
+  }
 `;
 
 const RoomTitle = styled.h3`
-  margin-left: .6rem;
+  margin-left: 0.6rem;
   color: #333;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2),
     -2px -2px 4px rgba(255, 255, 255, 0.2);
@@ -69,7 +74,7 @@ const ListedRoom = styled.div`
   background-color: #fff;
   border-bottom: 2px solid lightgray;
   padding: 0.5rem 1rem;
-  margin-left: .5rem;
+  margin-left: 0.5rem;
   cursor: pointer;
   width: 8rem;
   &:hover {
@@ -81,7 +86,7 @@ const ListedRoom = styled.div`
 const ToggleButton = styled.button`
   position: absolute;
   top: 0.5rem;
-  right: .8rem;
+  right: 0.8rem;
   background-color: transparent;
   border: none;
   font-size: 1.4rem;
